c40c2e076341c8380b29bb9bde02a510168735cd
Sonu-Buddh-Linkwok <sonu@linkwok.com>
2019-01-04 11:54:31 +0530

    add qty in phenol and acetone

--- app/controllers/api/v1/data_getter_controller.rb
+++ app/controllers/api/v1/data_getter_controller.rb
@@ -93,7 +93,7 @@ class Api::V1::DataGetterController < ApplicationController
 		sales_end = sales_start + 7.days
 
 
-		render json:{data: {'plant':[{'name':'Phenol',"qty":2588.54,'operating_rate':88.21,'downtime_hours':0.00,'onstream_hours':168.00},{'name':'Cumene',"qty":3296.9282,'operating_rate':0.00,'downtime_hours':0.00,'onstream_hours':0.00}],'other':[{'name':'Acetone',"qty":1557.98},{'name':'Benzene Drag',"qty":3077.73},{'name':'AWS',"qty":140.40}]}, success: true,message:""}
+		render json:{data: {'plant':[{'name': 'Phenol',"qty": 2588.54,'operating_rate':88.21,'downtime_hours':0.00,'onstream_hours':168.00},{'name':'Cumene',"qty":3296.9282,'operating_rate':0.00,'downtime_hours':0.00,'onstream_hours':0.00}],'other':[{'name':'Acetone',"qty":1557.98},{'name':'Benzene Drag',"qty":3077.73},{'name':'AWS',"qty":140.40}]}, success: true,message:""}
 
 	end
 	def sales
@@ -131,7 +131,7 @@ class Api::V1::DataGetterController < ApplicationController
 
 		heives_other_zone = 	SalesOutbound.where(date: Date.yesterday.strftime('%d-%m-%Y'),product:Product.where(name:['Heavies']),region: 'other').sum(:metric_tons).round(2)
 
-		render json:{data: {'zone':[{'name': 'Phenol': sales_phenol, 'north_qty': phenol_north_zone, 'west_qty': phenol_west_zone, 'south_qty': phenol_south_zone, 'east_qty': phenol_east_zone, 'central_qty': phenol_central_zone,'export_qty': phenol_export_zone},{'name': 'Acetone', : sales_acetone,'north_qty': acetone_north_zone,'west_qty': acetone_west_zone, 'south_qty': acetone_south_zone, 'east_qty' : acetone_east_zone,'central_qty': acetone_east_zone,'export_qty': acetone_export_zone}],'other':[{'name':'Heavies','qty': heives_other_zone}]}, success: true,message:""}
+		render json:{data: {'zone':[{'name': 'Phenol',"qty": sales_phenol, 'north_qty': phenol_north_zone, 'west_qty': phenol_west_zone, 'south_qty': phenol_south_zone, 'east_qty': phenol_east_zone, 'central_qty': phenol_central_zone,'export_qty': phenol_export_zone},{'name': 'Acetone',"qty", : sales_acetone,'north_qty': acetone_north_zone,'west_qty': acetone_west_zone, 'south_qty': acetone_south_zone, 'east_qty' : acetone_east_zone,'central_qty': acetone_east_zone,'export_qty': acetone_export_zone}],'other':[{'name':'Heavies','qty': heives_other_zone}]}, success: true,message:""}
 
 	end
 	def index
